5f20b69f003387be9428616ac0c5ceee9b508a20
LaluFarhan04 <240306047.mhs@uinmataram.ac.id>
2026-06-11 17:11:08 +0800

    Add initial HTML structure for index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pencarian Kos</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Web Pencarian Kos</h1>
+        <a href="admin/login.php">Login Admin</a> |
+        <a href="user/login.php">Login User</a> |
+        <a href="user/register.php">Register User</a>
+    </div>
+</body>
+</html>
